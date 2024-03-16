af32bf9efa0adf008a4aa3926c757be93f67fd62
Maryam <maryamkamareh1995@gmail.com>
2024-03-16 08:33:28 +0330

    add input data to project.

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 name = "maryam"
 family = "kamareh"
 print (name + family)
+
+# input
+print("how many kms?")
+kms = input()
+#ms = float(input())
+miles = float(kms)/1.6
+miles = round(miles, 2) # 2 ragham ashar
+print(f"ok converted data is {kms}.")
